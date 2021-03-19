58412523bd14bb622e843940ce8cb1956949af86
rjrodger <richard@ricebridge.com>
2021-03-19 15:11:50 +0000

    initial

---
+++ vue-2/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Jsonic</h1>
+    <pre>{{ json }}</pre>
+  </div>
+</template>
+
+<script>
+import { Jsonic } from 'jsonic'
+
+export default {
+  data () {
+    return {
+      json: Jsonic('a:1')
+    }
+  }
+}
+</script>
